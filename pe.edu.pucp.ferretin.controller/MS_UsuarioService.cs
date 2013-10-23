514b119c0c9ebd3f7cd313231300eed1b5eba9a5
LuisEspinoza212 <l.espinoza212@gmail.com>
2013-10-23 04:58:25 -0700

    Login hardcodeado admin admin

---
+++ pe.edu.pucp.ferretin.controller/MS_UsuarioService.cs
@@ -0,0 +1,43 @@
+﻿using pe.edu.pucp.ferretin.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.edu.pucp.ferretin.controller
+{
+    public static class MS_UsuarioService
+    {
+
+        private static FerretinDataContext db = new FerretinDataContext();
+
+        private static IEnumerable<Usuario> _listaUsuarios = null;
+
+        private static IEnumerable<Usuario> listaUsuarios
+        {
+            get
+            {
+                if (_listaUsuarios == null)
+                {
+                    obtenerListaUsuarios();
+                }
+                return _listaUsuarios;
+            }
+            set
+            {
+                _listaUsuarios = value;
+            }
+        }
+
+        public static IEnumerable<Usuario> obtenerListaUsuarios()
+        {
+            listaUsuarios = from p in db.Usuarios
+                            orderby p.dni
+                            select p;
+            return listaUsuarios;
+        }
+
+
+    }
+}
