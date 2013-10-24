1a22edcbb703f0365ec422eafdf9e40ea53d40ae
LuisEspinoza212 <l.espinoza212@gmail.com>
2013-10-24 14:55:20 -0700

    Parametros desde base de datos

---
+++ pe.edu.pucp.ferretin.controller/MS_ParametroService.cs
@@ -0,0 +1,40 @@
+﻿using pe.edu.pucp.ferretin.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.edu.pucp.ferretin.controller
+{
+    public static class MS_ParametroService
+    {
+        private static FerretinDataContext db = new FerretinDataContext();
+
+        private static IEnumerable<Parametro> _listaParametros = null;
+
+        private static IEnumerable<Parametro> listaParametros
+        {
+            get
+            {
+                if (_listaParametros == null)
+                {
+                    _listaParametros = obtenerListaParametros();
+                }
+                return _listaParametros;
+            }
+            set
+            {
+                _listaParametros = value;
+            }
+        }
+
+        public static IEnumerable<Parametro> obtenerListaParametros()
+        {
+            listaParametros = from p in db.Parametro
+                            orderby p.id
+                            select p;
+            return listaParametros;
+        }
+    }
+}
--- pe.edu.pucp.ferretin.view/MSeguridad/MS_ParametrosWindow.xaml.cs
+++ pe.edu.pucp.ferretin.view/MSeguridad/MS_ParametrosWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using pe.edu.pucp.ferretin.controller;
+using pe.edu.pucp.ferretin.model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,10 +21,27 @@ namespace pe.edu.pucp.ferretin.view.MSeguridad
     /// </summary>
     public partial class MS_ParametrosWindow : Window
     {
+        List<Parametro> listaParametros;
+
         public MS_ParametrosWindow()
         {
             InitializeComponent();
+
+            listaParametros = MS_ParametroService.obtenerListaParametros().ToList();
+
+            intContrasena.Text = listaParametros[0].valor.ToString();
+            tMaxSesion.Text = listaParametros[1].valor.ToString();
+            durClave.Text = listaParametros[2].valor.ToString();
+            tipCambio.Text = listaParametros[3].valor.ToString();
+            igv.Text = listaParametros[4].valor.ToString();
+
+            vigProforma.Text = listaParametros[5].valor.ToString();
+            vigNotaCredito.Text = listaParametros[6].valor.ToString();
+            solesPunto.Text = listaParametros[7].valor.ToString();
+            
         }
+
+        
         
     }
 }
