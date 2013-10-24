fc87b566bd9f193c3a4023e4b748c4978f75f1ca
Juan Carlos <jcarloscond@gmail.com>
2013-10-24 00:42:54 -0500

    Agregue modulo compras

---
+++ pe.edu.pucp.ferretin.view/MCompras/MC_AdministrarGuiaRemiWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace pe.edu.pucp.ferretin.view.MCompras
+{
+    /// <summary>
+    /// Interaction logic for MC_AdministrarGuiaRemiWindow.xaml
+    /// </summary>
+    public partial class MC_AdministrarGuiaRemiWindow : Window
+    {
+        public MC_AdministrarGuiaRemiWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
