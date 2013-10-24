f816e473a29177767bbd11a7868c43bd77454403
JoseGustavo <luther1911@gmail.com>
2013-10-24 11:58:41 -0500

    Busqueda en usuarios

--- pe.edu.pucp.ferretin.controller/MS_UsuarioService.cs
+++ pe.edu.pucp.ferretin.controller/MS_UsuarioService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace pe.edu.pucp.ferretin.controller
 {
@@ -20,7 +21,7 @@ namespace pe.edu.pucp.ferretin.controller
             {
                 if (_listaUsuarios == null)
                 {
-                    obtenerListaUsuarios();
+                    _listaUsuarios = obtenerListaUsuarios();
                 }
                 return _listaUsuarios;
             }
@@ -64,5 +65,64 @@ namespace pe.edu.pucp.ferretin.controller
             db.SubmitChanges();
         }
 
+        private static IEnumerable<Empleado> _listaEmpleados;
+        private static IEnumerable<Empleado> listaEmpleados
+        {
+            get
+            {
+                if (_listaEmpleados == null)
+                {
+                    _listaEmpleados = MR_EmpleadoService.obtenerListaEmpleados(); 
+                }
+                return _listaEmpleados;
+            }
+            set
+            {
+                _listaEmpleados = value;
+            }
+        }
+
+        public static System.Collections.IEnumerable obtenerListaUsuariosBy(Usuario usuario, Empleado empleado)
+        {
+            
+            return from u in listaUsuarios
+                   where
+                   (u.dni != null && u.dni.Contains(usuario.dni)
+                       && u.nombre != null && u.nombre.Contains(usuario.nombre)
+                       && u.Empleado != null && 
+                            ( u.Empleado.nombre != null && u.Empleado.nombre.Contains(empleado.nombre)
+                            && u.Empleado.apPaterno != null && u.Empleado.apPaterno.Contains(empleado.apPaterno)
+                            && u.Empleado.apMaterno != null && u.Empleado.apMaterno.Contains(empleado.apMaterno) 
+                            )
+                       && (usuario.id_perfil == 0 || ( u.id_perfil > 0 && u.id_perfil == usuario.id_perfil ) )
+                       && (usuario.estado == null  || ( u.estado == null && u.estado == usuario.estado ) )
+                    )
+                   orderby u.nombre
+                   select u;
+        }
+
+        private static IEnumerable<Perfil> _listaPerfiles;
+
+        private static IEnumerable<Perfil> listaPerfiles
+        {
+            get
+            {
+                if (_listaPerfiles == null)
+                {
+                    _listaPerfiles = from p in db.Perfil
+                                     select p;
+                }
+                return _listaPerfiles;
+            }
+            set
+            {
+                _listaPerfiles = value;
+            }
+        }
+
+        public static IEnumerable<Perfil> obtenerPerfiles()
+        {
+            return listaPerfiles;
+        }
     }
 }
