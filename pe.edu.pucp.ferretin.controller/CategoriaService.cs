c5d4c6977804fc15acff9f92bd69003b82a45411
CesarGarciaC <cdgarcia@pucp.pe>
2013-10-24 05:12:22 -0500

    Mant Productos

---
+++ pe.edu.pucp.ferretin.controller/CategoriaService.cs
@@ -0,0 +1,25 @@
+﻿using pe.edu.pucp.ferretin.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.edu.pucp.ferretin.controller
+{
+    public class CategoriaService
+    {
+        private static FerretinDataContextDataContext db = new FerretinDataContextDataContext();
+        private static IEnumerable<Categoria> listaCategoria=null;
+
+        public static IEnumerable<Categoria> obtenerTodasCategorias()
+        {
+            listaCategoria =
+                from c in db.Categoria
+                select c;
+
+            List<Categoria> listCat = listaCategoria.ToList();
+            return listCat;
+        }    
+    }
+}
