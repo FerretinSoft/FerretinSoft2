14199f632572216ca989ddadbff9a5ef007ecbc0
heidyhb <heidyhb89@gmail.com>
2013-10-24 01:18:55 -0500

    Se agregaron los proyectos para el modelo y el controlador. Se agrego el Data Context

---
+++ pe.edu.pucp.ferretin.controller/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace pe.edu.pucp.ferretin.controller
+{
+    public class Class1
+    {
+        
+
+        
+    }
+}
