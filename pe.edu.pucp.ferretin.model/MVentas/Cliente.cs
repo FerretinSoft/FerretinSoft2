4ad98166fdc59b6c8babe90f65f2a52da53f9c5f
JoseGustavo <luther1911@gmail.com>
2013-10-26 18:32:06 -0500

    add

---
+++ pe.edu.pucp.ferretin.model/MVentas/Cliente.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace pe.edu.pucp.ferretin.model
+{
+    public partial class Cliente
+    {
+
+        public int tipo
+        {
+            get
+            {
+                if (tipoDocumento == "DNI")
+                {
+                    return 1;
+                }
+                else if (tipoDocumento == "RUC")
+                {
+                    return 2;
+                }
+                else
+                    return 0;
+            }
+            set
+            {
+                if (value == 1)
+                {
+                    tipoDocumento = "DNI";
+                }
+                else if (value == 2)
+                {
+                    tipoDocumento = "RUC";
+                }
+                else
+                    tipoDocumento = "";
+                this.SendPropertyChanged("tipo");
+            }
+        }
+
+        public String nombreCompleto
+        {
+            get
+            {
+                return String.Join(" ", nombre, apPaterno, apMaterno);
+            }
+        }
+
+        partial void Ontelefono1Changing(String value)
+        {
+            Regex telefonoNumero = new Regex(@"^[2-9]\d{2}-\d{4}$");
+            if (telefonoNumero.IsMatch(value) == false)
+            {
+                throw new Exception("Numero de teléfono no válido");
+            }
+        }
+
+        partial void OnnroDocChanging(String value)
+        {
+            Int64 nroDoc = Int64.Parse(value);
+            if (nroDoc <= 0)
+            {
+                throw new Exception("El número de documento debe contener un valor numérico");
+            }
+            if (tipoDocumento.Equals("DNI") && (nroDoc>99999999 || nroDoc<10000000))
+            {
+                throw new Exception("El DNI debe tener exactamente 8 números");
+            }
+            if (tipoDocumento.Equals("RUC") && (nroDoc>99999999999 || nroDoc<10000000000))
+            {
+                throw new Exception("El RUC debe tener exactamente 11 números");
+            }
+        }
+    }
+}
---
+++ pe.edu.pucp.ferretin.viewmodel/Base/ViewModelBase.cs
@@ -0,0 +1,118 @@
+﻿using pe.edu.pucp.ferretin.controller;
+using pe.edu.pucp.ferretin.model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.edu.pucp.ferretin.viewmodel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged Members
+
+        //Instance
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            //this.VerifyPropertyName(propertyName);
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        //Instance .Net 4.5
+        public void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion INotifyPropertyChanged Members
+
+
+        public UbigeoDistrito selectedDistrito { get; set; }
+        private UbigeoDepartamento _selectedDepartamento;
+        public UbigeoDepartamento selectedDepartamento
+        {
+            get
+            {
+                return _selectedDepartamento;
+            }
+            set
+            {
+                _selectedDepartamento = value;
+                NotifyPropertyChanged("selectedDepartamento");
+                provincias = from d in ComunService.provincias where d.id_ubig_departamento == value.id select d;
+            }
+        }
+
+
+        private UbigeoProvincia _selectedProvincia;
+        public UbigeoProvincia selectedProvincia
+        {
+            get
+            {
+                return _selectedProvincia;
+            }
+            set
+            {
+                _selectedProvincia = value;
+                NotifyPropertyChanged("selectedProvincia");
+                distritos = from d in ComunService.distritos where d.id_ubig_provincia == value.id select d;
+            }
+        }
+
+        private IEnumerable<UbigeoDepartamento> _departamentos;
+        public IEnumerable<UbigeoDepartamento> departamentos
+        {
+            get
+            {
+                if (_departamentos == null)
+                {
+                    _departamentos = ComunService.departamentos;
+                }
+                return _departamentos;
+            }
+            set
+            {
+                _departamentos = value;
+                NotifyPropertyChanged("departamentos");
+            }
+        }
+        private IEnumerable<UbigeoProvincia> _provincias;
+        public IEnumerable<UbigeoProvincia> provincias
+        {
+            get
+            {
+                return _provincias;
+            }
+            set
+            {
+                _provincias = value;
+                NotifyPropertyChanged("provincias");
+            }
+        }
+
+        private IEnumerable<UbigeoDistrito> _distritos;
+        public IEnumerable<UbigeoDistrito> distritos
+        {
+            get
+            {
+                return _distritos;
+            }
+            set
+            {
+                _distritos = value;
+                NotifyPropertyChanged("distritos");
+            }
+        }
+    }
+}
