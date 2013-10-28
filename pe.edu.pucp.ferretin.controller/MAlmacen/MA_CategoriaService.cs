68411733e00b2c26cb244783ad2f51011ca97180
JoseGustavo <luther1911@gmail.com>
2013-10-27 21:23:01 -0500

    Manteniminento categorias

--- pe.edu.pucp.ferretin.controller/MAlmacen/MA_CategoriaService.cs
+++ pe.edu.pucp.ferretin.controller/MAlmacen/MA_CategoriaService.cs
@@ -1,6 +1,7 @@
 ﻿using pe.edu.pucp.ferretin.model;
 using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,30 @@ namespace pe.edu.pucp.ferretin.controller.MAlmacen
     public class MA_CategoriaService : MA_ComunService
     {
         
-        private static IEnumerable<Categoria> listaCategoria=null;
+        private static IEnumerable<Categoria> _listaCategoria=null;
 
-        public static IEnumerable<Categoria> obtenerTodasCategorias()
+        public static IEnumerable<Categoria> categorias
         {
+            get
+            {
+                if (_listaCategoria == null) _listaCategoria = db.Categoria;
 
-            listaCategoria =
-                from c in db.Categoria
-                select c;
-            List<Categoria> listCat = listaCategoria.ToList();
-            return listCat;
-        }    
+                return _listaCategoria;
+            }
+        }
+
+        public static bool insertarCategoria(Categoria categoria)
+        {
+            if (!db.Categoria.Contains(categoria))
+            {
+                db.Categoria.InsertOnSubmit(categoria);
+                return enviarCambios();
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
