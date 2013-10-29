f3352c07ea65fabe2476bfa7c0489c5c436e10c7
aPiza <angel.pizarro@pucp.pe>
2013-10-29 10:21:45 -0500

    AdministrarUsuariosViewModel

--- pe.edu.pucp.ferretin.controller/MSeguridad/MS_UsuarioService.cs
+++ pe.edu.pucp.ferretin.controller/MSeguridad/MS_UsuarioService.cs
@@ -1,27 +1,33 @@
-﻿using pe.edu.pucp.ferretin.model;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using System.Data.Linq;
+
 using pe.edu.pucp.ferretin.controller.MRecursosHumanos;
+using pe.edu.pucp.ferretin.model;
 
 namespace pe.edu.pucp.ferretin.controller.MSeguridad
 {
     public class MS_UsuarioService : MS_ComunService
     {
 
-        private static IEnumerable<Usuario> _listaUsuarios = null;
-
-        private static IEnumerable<Usuario> listaUsuarios
+        /*******************************************************/
+        public static IEnumerable<Usuario> _listaUsuarios = null;
+        public static IEnumerable<Usuario> listaUsuarios
         {
             get
             {
                 if (_listaUsuarios == null)
                 {
-                    _listaUsuarios = obtenerListaUsuarios();
+                    _listaUsuarios = db.Usuario;
                 }
+                //Usando concurrencia pesimista:
+                ///La lista de clientes se actualizara para ver los cambios
+                ///Si quisiera usar concurrencia optimista quito la siguiente linea
+                db.Refresh(RefreshMode.OverwriteCurrentValues, _listaUsuarios);
                 return _listaUsuarios;
             }
             set
@@ -29,11 +35,11 @@ namespace pe.edu.pucp.ferretin.controller.MSeguridad
                 _listaUsuarios = value;
             }
         }
-
+        /*******************************************************/
         public static IEnumerable<Usuario> obtenerListaUsuarios()
         {
             listaUsuarios = from p in db.Usuario
-                            orderby p.Empleado.dni
+                            orderby p.id_empleado
                             select p;
             return listaUsuarios;
         }
@@ -43,7 +49,7 @@ namespace pe.edu.pucp.ferretin.controller.MSeguridad
         {
             return from c in listaUsuarios
                    where
-                   (c.Empleado.dni != null && c.Empleado.dni.Contains(usuario.Empleado.dni)
+                   (c.id_empleado != null && c.id_empleado == usuario.id_empleado
                        && c.nombre != null && c.nombre.ToLower().Contains(usuario.nombre.ToLower().Trim())
                        && c.contrasena != null && c.contrasena.Contains(usuario.contrasena)
                        /*&& c.id_perfil != null && c.id_perfil.Contains(usuario.id_perfil)
@@ -53,11 +59,11 @@ namespace pe.edu.pucp.ferretin.controller.MSeguridad
                    select c;
         }
         /*******************************************************/
-        public static void insertarUsuario(Usuario usuario)
-        {
-            db.Usuario.InsertOnSubmit(usuario);
-            db.SubmitChanges();
-        }
+        //public static void insertarUsuario(Usuario usuario)
+        //{
+        //    db.Usuario.InsertOnSubmit(usuario);
+        //    db.SubmitChanges();
+        //}
         /*******************************************************/
         public static void actualizarUsuario(Usuario usuario)
         {
@@ -71,7 +77,7 @@ namespace pe.edu.pucp.ferretin.controller.MSeguridad
             {
                 if (_listaEmpleados == null)
                 {
-                    _listaEmpleados = MR_EmpleadoService.obtenerListaEmpleados(); 
+                    _listaEmpleados = MR_EmpleadoService.obtenerListaEmpleados();
                 }
                 return _listaEmpleados;
             }
@@ -83,22 +89,57 @@ namespace pe.edu.pucp.ferretin.controller.MSeguridad
 
         public static System.Collections.IEnumerable obtenerListaUsuariosBy(Usuario usuario, Empleado empleado)
         {
-            
+
             return from u in listaUsuarios
                    where
-                   (u.Empleado.dni != null && u.Empleado.dni.Contains(usuario.Empleado.dni)
+                   (u.id_empleado != null && u.id_empleado == usuario.id_empleado
                        && u.nombre != null && u.nombre.ToLower().Contains(usuario.nombre.ToLower().Trim())
                        && u.Empleado != null &&
                             (u.Empleado.nombre != null && u.Empleado.nombre.ToLower().Contains(empleado.nombre.ToLower().Trim())
                             && u.Empleado.apPaterno != null && u.Empleado.apPaterno.ToLower().Contains(empleado.apPaterno.ToLower().Trim())
-                            //&& u.Empleado.apMaterno != null && u.Empleado.apMaterno.ToLower().Contains(empleado.apMaterno.ToLower().Trim()) 
+                       //&& u.Empleado.apMaterno != null && u.Empleado.apMaterno.ToLower().Contains(empleado.apMaterno.ToLower().Trim()) 
                             )
-                       && (usuario.id_perfil == null || ( u.id_perfil != null && u.id_perfil.Equals(usuario.id_perfil)))
+                       && (usuario.id_perfil == null || (u.id_perfil != null && u.id_perfil.Equals(usuario.id_perfil)))
                        && (usuario.estado == null || (u.estado != null && u.estado.Equals(usuario.estado)))
                     )
                    orderby u.nombre
                    select u;
         }
+
+        /*******************************************************/
+        public static IEnumerable<Usuario> buscar(string codigo, string nomUsuario, int perfil, string nombres, string apellidos, int estado)
+        {
+            return from u in listaUsuarios
+                   where (
+                       //Cada fila es un filtro
+                   (u.id_empleado != null && u.id_empleado == perfil
+                       && u.nombre != null && u.nombre.ToLower().Contains(nomUsuario.ToLower().Trim())
+                       && (perfil == 0 || (u.id_perfil != null && u.id_perfil.Equals(perfil)))
+                       && u.Empleado != null &&
+                            (u.Empleado.nombre != null && u.Empleado.nombre.ToLower().Contains(nombres.ToLower().Trim())
+                            && u.Empleado.apPaterno != null && u.Empleado.apPaterno.ToLower().Contains(apellidos.ToLower().Trim())
+                       //&& u.Empleado.apMaterno != null && u.Empleado.apMaterno.ToLower().Contains(empleado.apMaterno.ToLower().Trim()) 
+                            )
+                       && (estado == 0 || (u.estado != null && u.estado.Equals(estado == 1 ? true : false)))
+                    ))
+                   orderby u.nombre
+                   select u;
+        }
+        /*******************************************************/
+        public static bool insertarUsuario(Usuario usuario)
+        {
+            if (!db.Usuario.Contains(usuario))
+            {
+                db.Usuario.InsertOnSubmit(usuario);
+                return enviarCambios();
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /*******************************************************/
 
         private static IEnumerable<Perfil> _listaPerfiles;
 
---
+++ pe.edu.pucp.ferretin.viewmodel/MSeguridad/MS_AdministrarUsuariosViewModel.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+using pe.edu.pucp.ferretin.controller;
+using pe.edu.pucp.ferretin.controller.MSeguridad;
+using pe.edu.pucp.ferretin.controller.MRecursosHumanos;
+using pe.edu.pucp.ferretin.model;
+using pe.edu.pucp.ferretin.viewmodel.Helper;
+
+namespace pe.edu.pucp.ferretin.viewmodel.MSeguridad
+{
+    public class MS_AdministrarUsuariosViewModel : ViewModelBase
+    {
+        #region Valores para el cuadro de Búsqueda
+        private String _searchCodigo = "";
+        public String searchCodigo { get { return _searchCodigo; } set { _searchCodigo = value; } }
+
+        private String _searchNombreUsuario = "";
+        public String searchNombreUsuario { get { return _searchNombreUsuario; } set { _searchNombreUsuario = value; } }
+
+        private int _searchPerfil = 0;
+        public int searchPerfil { get { return _searchPerfil; } set { _searchPerfil = value; NotifyPropertyChanged("searchPerfil"); } }
+
+        private String _searchNombres = "";
+        public String searchNombres { get { return _searchNombres; } set { _searchNombres = value; } }
+
+        private String _searchApellidos = "";
+        public String searchApellidos { get { return _searchApellidos; } set { _searchApellidos = value; } }
+
+        private int _searchEstado = 0;
+        public int searchEstado { get { return _searchEstado; } set { _searchEstado = value; NotifyPropertyChanged("searchEstado"); } }
+        #endregion
+
+
+        #region Manejo de los Tabs
+        public enum tabs
+        {
+            //Pestañas virtuales:
+            //0       1        2          3
+            BUSQUEDA, AGREGAR, MODIFICAR, DETALLES
+        }
+        private tabs _statusTab = tabs.BUSQUEDA; //pestaña default 
+        public tabs statusTab
+        {
+            get
+            {
+                return _statusTab;
+            }
+            set
+            {
+                _statusTab = value;
+                //Si cambió el estado de las pestañas también cambio los Header
+                //Si la pestaña es para agregar nuevo, limpio los input
+                switch (value)
+                {
+                    case tabs.BUSQUEDA: detallesTabHeader = "Agregar"; usuario = new Usuario(); break;//Si es agregar, creo un nuevo objeto Usuario
+                    case tabs.AGREGAR: detallesTabHeader = "Agregar"; usuario = new Usuario(); break;//Si es agregar, creo un nuevo objeto Usuario
+                    case tabs.MODIFICAR: detallesTabHeader = "Modificar"; break;
+                    case tabs.DETALLES: detallesTabHeader = "Detalles"; break;
+                    default: detallesTabHeader = "Agregar"; usuario = new Usuario(); break;//Si es agregar, creo un nuevo objeto Cliente
+                }
+                //Cuando se cambia el status, tambien se tiene que cambiar el currentIndex del tab
+                //currentIndexTab = _statusTab == 0 ? 0 : 1;
+                NotifyPropertyChanged("statusTab");
+                //Cuando se cambia el status, tambien se tiene que actualizar el currentIndex del tab
+                NotifyPropertyChanged("currentIndexTab"); //Hace que cambie el tab automaticamente
+            }
+        }
+        //Usado para mover los tabs de acuerdo a las acciones realizadas
+        public int currentIndexTab
+        {
+            get { return _statusTab == tabs.BUSQUEDA ? 0 : 1; }
+            set { statusTab = value == 0 ? tabs.BUSQUEDA : tabs.AGREGAR; }
+
+        }
+
+        private String _detallesTabHeader = "Agregar"; //Default
+        public String detallesTabHeader
+        {
+            get
+            {
+                return _detallesTabHeader;
+            }
+            set
+            {
+                _detallesTabHeader = value;
+                NotifyPropertyChanged("detallesTabHeader");
+            }
+        }
+        #endregion
+
+        #region Lista de Usuarios y Edición de Usuarios
+        /**************************************************/
+        private Usuario _usuario = new Usuario();
+        public Usuario usuario
+        {
+            get
+            {
+                return _usuario;
+            }
+            set
+            {
+                _usuario = value;
+                NotifyPropertyChanged("usuario");
+            }
+        }
+        /**************************************************/
+        public IEnumerable<Perfil> perfiles
+        {
+            get
+            {
+                return MS_UsuarioService.obtenerPerfiles();
+            }
+        }
+        /**************************************************/
+        private IEnumerable<Usuario> _listaUsuarios;
+        public IEnumerable<Usuario> listaUsuarios
+        {
+            get
+            {
+                _listaUsuarios = MS_UsuarioService.buscar(searchCodigo, searchNombreUsuario, searchPerfil, searchNombres, searchApellidos, searchEstado);
+                return _listaUsuarios;
+            }
+            set
+            {
+                _listaUsuarios = value;
+                NotifyPropertyChanged("listaUsuarios");
+            }
+        }
+        /**************************************************/
+        #endregion
+        
+        #region RalayCommand
+        RelayCommand _actualizarListaUsuariosCommand;
+        public ICommand actualizarListaUsuariosCommand
+        {
+            get
+            {
+                if (_actualizarListaUsuariosCommand == null)
+                {
+                    _actualizarListaUsuariosCommand = new RelayCommand(param => NotifyPropertyChanged("listaUsuarios"));
+                }
+                return _actualizarListaUsuariosCommand;
+            }
+        }
+        RelayCommand _agregarUsuarioCommand;
+        public ICommand agregarUsuarioCommand
+        {
+            get
+            {
+                if (_agregarUsuarioCommand == null)
+                {
+                    _agregarUsuarioCommand = new RelayCommand(p => statusTab = tabs.AGREGAR);
+                }
+                return _agregarUsuarioCommand;
+            }
+        }
+        RelayCommand _viewEditUsuarioCommand;
+        public ICommand viewEditUsuarioCommand
+        {
+            get
+            {
+                if (_viewEditUsuarioCommand == null)
+                {
+                    _viewEditUsuarioCommand = new RelayCommand(viewEditUsuario);
+                }
+                return _viewEditUsuarioCommand;
+            }
+        }
+        RelayCommand _saveUsuarioCommand;
+        public ICommand saveUsuarioCommand
+        {
+            get
+            {
+                if (_saveUsuarioCommand == null)
+                {
+                    _saveUsuarioCommand = new RelayCommand(saveUsuario);
+                }
+                return _saveUsuarioCommand;
+            }
+        }
+        RelayCommand _cancelUsuarioCommand;
+        public ICommand cancelUsuarioCommand
+        {
+            get
+            {
+                if (_cancelUsuarioCommand == null)
+                {
+                    _cancelUsuarioCommand = new RelayCommand(cancelUsuario);
+                }
+                return _cancelUsuarioCommand;
+            }
+        }
+        #endregion
+
+        #region Comandos
+        public void viewEditUsuario(Object id)
+        {
+            try
+            {
+                //this.usuario = listaAlmacenes.Single(almacen => almacen.id == (short)id);
+                //if (this.almacen.id_ubigeo != null)
+                //{
+                //    selectedProvincia = this.almacen.UbigeoDistrito.UbigeoProvincia;
+                //    selectedDepartamento = selectedProvincia.UbigeoDepartamento;
+                //}
+                this.statusTab = tabs.MODIFICAR;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        public void saveUsuario(Object obj)
+        {
+
+            if (usuario.id_empleado != null)//Si existe
+            {
+                if (!MS_UsuarioService.enviarCambios())
+                {
+                    MessageBox.Show("No se pudo actualizar el usuario");
+                }
+                else
+                {
+                    MessageBox.Show("El usuario fue guardado con éxito");
+                }
+            }
+            else
+            {
+                if (!MS_UsuarioService.insertarUsuario(usuario))
+                {
+                    MessageBox.Show("No se pudo agregar el nuevo usuario");
+                }
+                else
+                {
+                    MessageBox.Show("El usuario fue agregado con éxito");
+                }
+            }
+            NotifyPropertyChanged("listaAlmacenes");
+        }
+        public void cancelUsuario(Object obj)
+        {
+            this.statusTab = tabs.BUSQUEDA;
+            listaUsuarios = MS_UsuarioService.listaUsuarios;
+        }
+        #endregion
+
+    }
+}
+
