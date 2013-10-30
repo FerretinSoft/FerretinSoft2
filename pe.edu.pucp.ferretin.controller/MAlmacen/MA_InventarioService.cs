fb77e6cfecde8bd15c9d68f0b06263308f93a2bf
Juan Carlos <jcarloscond@gmail.com>
2013-10-30 12:48:00 -0500

    InventarioService

---
+++ pe.edu.pucp.ferretin.controller/MAlmacen/MA_InventarioService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.edu.pucp.ferretin.controller.MAlmacen
+{
+    class MA_InventarioService
+    {
+    }
+}
