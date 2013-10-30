807cacb39fa0e0d0fc327b1525d13fc5b35d59f8
heidyhb <heidyhb89@gmail.com>
2013-10-29 20:15:33 -0500

    Cambios en el servicio de Movimientos (controlador)

--- pe.edu.pucp.ferretin.controller/MAlmacen/MA_MovimientosService.cs
+++ pe.edu.pucp.ferretin.controller/MAlmacen/MA_MovimientosService.cs
@@ -10,9 +10,7 @@ namespace pe.edu.pucp.ferretin.controller.MAlmacen
 {
     public class MA_MovimientosService : MA_ComunService
     {
-        static FerretinDataContext dc = new FerretinDataContext();
-
-        private static IEnumerable<Movimiento> _listaMovimientos = null;
+       private static IEnumerable<Movimiento> _listaMovimientos;
 
         private static IEnumerable<Movimiento> listaMovimientos
         {
@@ -20,8 +18,13 @@ namespace pe.edu.pucp.ferretin.controller.MAlmacen
             {
                 if (_listaMovimientos == null)
                 {
-                    ObtenerListaMovimientos();
+                    _listaMovimientos = db.Movimiento;
                 }
+                
+                //Usando concurrencia pesimista:
+                ///La lista de movimientos se actualizara para ver los cambios
+                ///Si quisiera usar concurrencia optimista quito la siguiente linea
+                db.Refresh(RefreshMode.OverwriteCurrentValues, _listaMovimientos);
                 return _listaMovimientos;
             }
             set
@@ -30,16 +33,14 @@ namespace pe.edu.pucp.ferretin.controller.MAlmacen
             }
         }
 
-        public static IEnumerable<Movimiento> ObtenerListaMovimientos()
+        public static Movimiento ObtenerMovimientoPorId(int id)
         {
-            listaMovimientos = from mov in dc.Movimiento
-                            orderby mov.fecha
-                            select mov;
-            return listaMovimientos;
+            Movimiento movimiento = (from m in listaMovimientos
+                               where m.id.Equals(id)
+                               select m).Single();
+
+            return movimiento;
         }
-
-        
-
 
         public static IEnumerable<Movimiento> ObtenerListaMovimientos(Dictionary<string, object> parametros)
         {
@@ -55,14 +56,20 @@ namespace pe.edu.pucp.ferretin.controller.MAlmacen
         public static void ActualizarMovimiento(Movimiento mov)
         {
             //dc.Movimiento.InsertOnSubmit(mov);
-            dc.SubmitChanges();
+            //db.SubmitChanges();
         }
 
-        public static void InsertarMovimiento(Movimiento mov)
+        public static bool insertarMovimiento(Movimiento movimiento)
         {
-            //mov.Movimiento_Tipo;
-            dc.Movimiento.InsertOnSubmit(mov);
-            dc.SubmitChanges();
+            if (!db.Movimiento.Contains(movimiento))
+            {
+                db.Movimiento.InsertOnSubmit(movimiento);
+                return enviarCambios();
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
