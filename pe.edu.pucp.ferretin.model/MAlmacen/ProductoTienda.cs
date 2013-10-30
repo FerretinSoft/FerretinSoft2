3ca8763a6b8c9ebc1c2a45ddd8b6875da3d19cdc
Juan Carlos <jcarloscond@gmail.com>
2013-10-30 12:34:54 -0500

    Model ProductoTienda

---
+++ pe.edu.pucp.ferretin.model/MAlmacen/ProductoTienda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.edu.pucp.ferretin.model.MAlmacen
+{
+    class ProductoTienda
+    {
+        Producto prod { get; set; }
+        ProductoAlmacen prodAlm { get; set; }
+        ProductoCategoria prodCat { get; set; }
+        Tienda tiend { get; set; }
+        Categoria cat { get; set; }
+    }
+}
