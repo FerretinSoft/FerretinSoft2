49a83b83a37cb6d79d27c665bbf052c53d4a9ccf
heidyhb <heidyhb89@gmail.com>
2013-10-31 02:15:52 -0500

    Avances en mantenimiento de solicitudes de abastecimiento.

--- pe.edu.pucp.ferretin.controller/MAlmacen/MA_SharedService.cs
+++ pe.edu.pucp.ferretin.controller/MAlmacen/MA_SharedService.cs
@@ -39,5 +39,19 @@ namespace pe.edu.pucp.ferretin.controller.MAlmacen
                 _tiposMovimiento = value;
             }
         }
+
+        private static List<SolicitudAbastecimientoEstado> _estadosSolicitud;
+        public static List<SolicitudAbastecimientoEstado> estadosSolicitud
+        {
+            get
+            {
+                if (_estadosSolicitud == null) _estadosSolicitud = db.SolicitudAbastecimientoEstado.ToList();
+                return _estadosSolicitud;
+            }
+            set
+            {
+                _estadosSolicitud = value;
+            }
+        }
     }
 }
---
+++ pe.edu.pucp.ferretin.view/MAlmacen/MA_RegistroSolAbastecimientoWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace pe.edu.pucp.ferretin.view.MAlmacen
+{
+    /// <summary>
+    /// Lógica de interacción para MA_SolicitudAbastecimientoWindow.xaml
+    /// </summary>
+    public partial class MA_RegistroSolAbastecimientoWindow : Window
+    {
+        public MA_RegistroSolAbastecimientoWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void atenderSolBtn_Click(object sender, RoutedEventArgs e)
+        {
+            solicitudesTab.SelectedIndex = 3;
+        }
+
+        private void buscarSolBtn_Click(object sender, RoutedEventArgs e)
+        {
+            solicitudesTab.SelectedIndex = 0;
+        }
+
+        private void consolidarBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MCompras.MC_ConsolidarSolicitudesWindow consoli = new MCompras.MC_ConsolidarSolicitudesWindow();
+            consoli.Show();
+        }
+    }
+}
