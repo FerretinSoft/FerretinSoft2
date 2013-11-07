39873986361a6890e3b370a6fbfc1e00451cae82
JoseGustavo <luther1911@gmail.com>
2013-11-07 01:04:30 -0500

    proformas

--- pe.edu.pucp.ferretin.controller/MVentas/MV_ProformasService.cs
+++ pe.edu.pucp.ferretin.controller/MVentas/MV_ProformasService.cs
@@ -10,14 +10,59 @@ namespace pe.edu.pucp.ferretin.controller.MVentas
     public class MV_ProformasService : MV_ComunService
     {
         public static bool insertarProforma(Proforma proforma){
-            if (!db.Proforma.Contains(proforma))
+            if (!listaProformas.Contains(proforma))
             {
+                proforma.codigo = newCodProforma;
                 db.Proforma.InsertOnSubmit(proforma);
                 return enviarCambios();
             }
             else
             {
                 return false;
+            }
+        }
+
+        private static IEnumerable<Proforma> _listaProformas;
+        private static IEnumerable<Proforma> listaProformas
+        {
+            get
+            {
+                if (_listaProformas == null)
+                {
+                    _listaProformas = db.Proforma;
+                }
+                
+                return _listaProformas;
+            }
+            set
+            {
+                _listaProformas = value;
+            }
+        }
+
+        public static IEnumerable<Proforma> buscarProformas(string codProformaSearch, Usuario usuarioSearch, Cliente clienteSearch, DateTime fechaDesdeSearch, DateTime fechaHastaSearch)
+        {
+            
+            codProformaSearch = codProformaSearch==null?String.Empty:codProformaSearch.ToLower();
+
+            return from p in listaProformas select p;
+        }
+
+
+        public static string newCodProforma
+        {
+            get
+            {
+                Int64 numCodProf = listaProformas.Max(p=>p.id) + 1;
+                string codDev = Convert.ToString(numCodProf);
+                while (true)
+                {
+                    if (codDev.Length == 8)
+                        break;
+                    else
+                        codDev = "0" + codDev;
+                }
+                return codDev+"-"+DateTime.Now.Year.ToString();
             }
         }
     }
---
+++ pe.edu.pucp.ferretin.model/MVentas/ProformaProducto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.edu.pucp.ferretin.model
+{
+    public partial class ProformaProducto
+    {
+        partial void OncantidadChanged()
+        {
+            //Cantidad no puede ser negativa
+            if (cantidad < 0) cantidad = 0;
+
+            
+
+            //Busco si tiene promocion, aplico el descuento
+            if (PromocionActual != null)
+            {
+                if (cantidad >= PromocionActual.cantMulUnidades)
+                {
+                    descuentoPorcentaje = PromocionActual.descuento;
+                }
+                else
+                {
+                    descuentoPorcentaje = 0;
+                }
+            }
+            else
+            {
+                descuentoPorcentaje = 0;
+            }
+
+            //Actualizo monto parcial
+            if (Producto != null)
+            {
+                decimal? desc = ((100 - (descuentoPorcentaje == null ? 0 : descuentoPorcentaje)) / 100);
+                //Calcular la cantidad de productos que tienen descuento
+                int? prodConDesc = ((PromocionActual != null && cantidad >= PromocionActual.cantMulUnidades) ? (cantidad / PromocionActual.cantMulUnidades) * PromocionActual.cantMulUnidades : 0);
+                //Calcular Si se ha superamo el maximo permitido por venta
+                prodConDesc = prodConDesc > 0 && prodConDesc > (PromocionActual.maxPromVenta * PromocionActual.cantMulUnidades) ? (PromocionActual.maxPromVenta * PromocionActual.cantMulUnidades) : prodConDesc;
+                //Calculo la cantidad de productos sin descuento
+                int? prodSinDesc = cantidad - prodConDesc;
+                //Hallo el resultado de los productos con descuento y sin descuento
+                descuento = (prodConDesc * Producto.precioLista * (1 - desc));
+                montoParcial = Decimal.Round((prodConDesc * Producto.precioLista * desc + prodSinDesc * Producto.precioLista).Value, 2);
+                montoReal = cantidad * Producto.precioLista;
+            }
+        }
+
+
+        public PromocionProducto PromocionActual { get; set; }
+    }
+}
