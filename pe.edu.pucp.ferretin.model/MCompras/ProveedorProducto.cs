f54ade9efa36c34da92d7e702f8636a3fbfe9bd7
croberto <crobertoyl@gmail.com>
2013-11-11 23:34:08 -0500

    checkbox oc

--- pe.edu.pucp.ferretin.model/MCompras/ProveedorProducto.cs
+++ pe.edu.pucp.ferretin.model/MCompras/ProveedorProducto.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace pe.edu.pucp.ferretin.model
 {
-    public partial class ProveedorProducto
+    public partial class ProveedorProducto : INotifyPropertyChanged
     {
         private bool _isSelected;
         public bool isSelected
@@ -19,7 +19,16 @@ namespace pe.edu.pucp.ferretin.model
             set
             {
                 _isSelected = value;
+                OnPropertyChanged("isSelected");
             }
         }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }       
     }
 }
