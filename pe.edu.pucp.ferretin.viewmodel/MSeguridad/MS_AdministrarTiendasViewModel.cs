e5268ce7190e03759ec894b200131c38ad681fc9
mmc01 <a20097102@pucp.edu.pe>
2013-11-12 19:52:33 -0500

    Validacion de distrito en Tienda

--- pe.edu.pucp.ferretin.viewmodel/MSeguridad/MS_AdministrarTiendasViewModel.cs
+++ pe.edu.pucp.ferretin.viewmodel/MSeguridad/MS_AdministrarTiendasViewModel.cs
@@ -446,6 +446,7 @@ namespace pe.edu.pucp.ferretin.viewmodel.MSeguridad
             if (tienda.nombre == null||(tienda.tipo == -1 || tienda.tipo == null)
                     ||tienda.direccion==null||tienda.telefono1==null|| "".Equals(tienda.direccion)
                     || "".Equals(tienda.nombre) || "".Equals(tienda.telefono1)
+                    ||tienda.id_ubigeo == null || "".Equals(tienda.id_ubigeo)
                     ||tienda.nombre==null||tienda.telefono1==null||
                     (tienda.tipo == 0 && tienda.Tienda1 == null))
             {                
