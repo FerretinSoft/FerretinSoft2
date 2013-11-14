015d66daf0fcac7861032f420e8a87c21d2d2c49
mmc01 <a20097102@pucp.edu.pe>
2013-11-13 19:06:01 -0500

    Prueba Tienda

--- pe.edu.pucp.ferretin.viewmodel/MSeguridad/MS_AdministrarTiendasViewModel.cs
+++ pe.edu.pucp.ferretin.viewmodel/MSeguridad/MS_AdministrarTiendasViewModel.cs
@@ -429,25 +429,30 @@ namespace pe.edu.pucp.ferretin.viewmodel.MSeguridad
         public ImageSource tiendaImagen
         {
             get
-            {                
-                if (this.almacen.foto != null)
+            {
+                try
                 {
-                    MemoryStream strm = new MemoryStream();
-                    strm.Write(almacen.foto.ToArray(), 0, almacen.foto.Length);
-                    strm.Position = 0;
-                    System.Drawing.Image img = System.Drawing.Image.FromStream(strm);
+                    if (this.almacen.foto != null)
+                    {
+                        MemoryStream strm = new MemoryStream();
+                        strm.Write(almacen.foto.ToArray(), 0, almacen.foto.Length);
+                        strm.Position = 0;
+                        System.Drawing.Image img = System.Drawing.Image.FromStream(strm);
 
-                    BitmapImage bitmapImage = new BitmapImage();
-                    bitmapImage.BeginInit();
-                    MemoryStream memoryStream = new MemoryStream();
-                    img.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
-                    memoryStream.Seek(0, SeekOrigin.Begin);
-                    bitmapImage.StreamSource = memoryStream;
-                    bitmapImage.EndInit();
+                        BitmapImage bitmapImage = new BitmapImage();
+                        bitmapImage.BeginInit();
+                        MemoryStream memoryStream = new MemoryStream();
+                        img.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                        memoryStream.Seek(0, SeekOrigin.Begin);
+                        bitmapImage.StreamSource = memoryStream;
+                        bitmapImage.EndInit();
 
-                    _tiendaImagen = bitmapImage;
-                }                 
-                 return _tiendaImagen;
+                        _tiendaImagen = bitmapImage;
+                    }
+                    return _tiendaImagen;
+                } catch(Exception e){
+                    return _tiendaImagen;
+                }
              
             }
             set
