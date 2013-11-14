0a56585a9a5aa1033ec6c557e28f92e490c8e46f
CesarGarciaC <cdgarcia@pucp.pe>
2013-11-14 04:45:14 -0500

    Otros cambios

---
+++ pe.edu.pucp.ferretin.view/MAlmacen/MA_ReporteKardexWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using pe.edu.pucp.ferretin.controller.MAlmacen;
+using pe.edu.pucp.ferretin.model;
+using System.Collections.ObjectModel;
+
+namespace pe.edu.pucp.ferretin.view.MAlmacen
+{
+    /// <summary>
+    /// Lógica de interacción para MA_ReporteKardexWindow.xaml
+    /// </summary>
+    public partial class MA_ReporteKardexWindow : Window
+    {
+        public MA_ReporteKardexWindow()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
---
+++ pe.edu.pucp.ferretin.viewmodel/MCompras/MC_AtenderSolicitudViewModel.cs
@@ -0,0 +1,100 @@
+﻿using pe.edu.pucp.ferretin.controller;
+using pe.edu.pucp.ferretin.controller.MAlmacen;
+using pe.edu.pucp.ferretin.controller.MCompras;
+using pe.edu.pucp.ferretin.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.edu.pucp.ferretin.viewmodel.MCompras
+{
+    public class MC_AtenderSolicitudViewModel : ViewModelBase
+    {
+        #region lista Productos de la  Solicitud
+        //private ProductoAlmacen _productoSol;
+        //public ProductoAlmacen productoSol
+        //{
+        //    get
+        //    {
+        //        return _productoSol;
+        //    }
+        //    set
+        //    {
+        //        _productoSol = value;
+        //        NotifyPropertyChanged("productoSol");
+        //    }
+        //}
+        //private Dictionary<ProductoAlmacen,decimal> _listaProductosSol;
+        //public Dictionary<ProductoAlmacen, decimal> listaProductosSol
+        //{
+        //    get
+        //    {   Usuario usuario = ComunService.usuarioL;
+        //        Tienda tienda = usuario.Empleado.tiendaActual;
+        //        _listaProductosSol = MA_SharedService.obtenerProductosPorAbastecer(tienda);
+        //        return _listaProductosSol;
+        //    }
+        //    set
+        //    {
+        //        _listaProductosSol = value;
+        //        NotifyPropertyChanged("listaProductosSol");
+                
+
+        //    }
+
+        //}
+
+        private ProductoSol _productoSol;
+        public ProductoSol productoSol
+        {
+            get
+            {
+                return _productoSol;
+            }
+            set
+            {
+                _productoSol = value;
+                NotifyPropertyChanged("productoSol");
+            }
+        }
+        private List<ProductoSol> _listaProductosSol;
+        public List<ProductoSol> listaProductosSol
+        {
+            get
+            {
+                _listaProductosSol = new List<ProductoSol>();
+                Usuario usuario = ComunService.usuarioL;
+                Tienda tienda = usuario.Empleado.tiendaActual;
+                ProductoSol actual;
+                foreach (var entry in MA_SharedService.obtenerProductosPorAbastecer(tienda))
+                {
+                    actual = new ProductoSol();
+                    actual.cantidad = entry.Value;
+                    actual.producto = entry.Key;
+                    _listaProductosSol.Add(actual);
+                    // do something with entry.Value or entry.Key                   
+                }
+                int i;
+                for (i = 0; i < _listaProductosSol.Count(); i++)
+                {
+                    _listaProductosSol[i].posiProveedor = MC_ProveedorService.obtenerPosiblesProveedores(_listaProductosSol[i].producto); 
+                }
+
+                return _listaProductosSol;
+            }
+            set
+            {
+                _listaProductosSol = value;
+                NotifyPropertyChanged("listaProductosSol");
+
+
+            }
+
+        }
+
+        #endregion
+
+
+    }
+} 
