e5d5786d78426d5a3bad15dc18c239f9904e52eb
LuisEspinoza212 <l.espinoza212@gmail.com>
2013-11-17 02:40:18 -0500

    ReportesReportesReportes Parametrizado

--- pe.edu.pucp.ferretin.view/MSeguridad/ReporteDeTransacciones.cs
+++ pe.edu.pucp.ferretin.view/MSeguridad/ReporteDeTransacciones.cs
@@ -103,6 +103,22 @@ namespace pe.edu.pucp.ferretin.view.MSeguridad {
                 return this.ReportDefinition.Sections[6];
             }
         }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_FechaIni {
+            get {
+                return this.DataDefinition.ParameterFields[0];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_Fechafin {
+            get {
+                return this.DataDefinition.ParameterFields[1];
+            }
+        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
