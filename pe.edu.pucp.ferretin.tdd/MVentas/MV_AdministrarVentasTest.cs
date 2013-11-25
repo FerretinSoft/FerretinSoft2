5de0b1cfb55a622ed60a5bf7c4074ee7adfd427e
JoseGustavo <luther1911@gmail.com>
2013-11-25 03:18:21 -0500

    tdd

--- pe.edu.pucp.ferretin.tdd/MVentas/MV_AdministrarVentasTest.cs
+++ pe.edu.pucp.ferretin.tdd/MVentas/MV_AdministrarVentasTest.cs
@@ -68,5 +68,42 @@ namespace pe.edu.pucp.ferretin.tdd.MVentas
                 Assert.AreEqual(puntGan,cantProd*ganPunt);                
             }
         }
+
+        [TestCase]
+        public void creacion_de_ventas_disminusion_de_stock()
+        {
+            var user = ComunService.db.Usuario.First(u => u.nombre.ToLower().Equals("jquilca"));
+            ComunService.usuarioLo(user);
+            var tienda = user.Empleado.tiendaActual;
+            for (int v = 0; v < 1000; v++)
+            {
+                ComunService.Clean();
+                var vm = new MV_RegistrarVentaViewModel();
+                var productos = ComunService.db.ProductoAlmacen.Where(pa => pa.Tienda.id.Equals(tienda.id) && pa.estado.HasValue && pa.estado == 1 && pa.Producto != null && pa.Producto.estado == 1).Take(8).ToList();
+                //var creo el entorno
+                int cantProd = productos.Count();
+                Console.WriteLine("Cantidad de prodcutos: " + cantProd.ToString());
+                int max = (int)(new Random()).Next(1, 10);
+                int cant = 0;
+                while (vm.venta.VentaProducto.Count < max && cant <= 10)
+                {
+                    int prod = (int)(new Random(v)).Next(0, cantProd - 1);
+
+                    var producto = productos.ElementAt(prod);
+                    if (producto.Producto.precioLista > 0 && producto.stock > 0)
+                    {
+                        vm.agregarProducto(producto);
+                        cant++;
+                        Console.WriteLine("Producto Agregado: " + producto.Producto.nombre.ToString());
+                    }
+                }
+                var vmPagar = new MV_PagoWindowViewModel();
+                vmPagar.venta = vm.venta;
+                vmPagar.agregarPago(1);//solo efectivo
+                vmPagar.imprimirDocumento(null);
+                Console.WriteLine("Venta Finalizada.");
+            }
+            Assert.AreEqual(0, 0);
+        }
     }
 }
